3140e6b7980dfd87de2b870f7e4a721f2f8e33c1
edemuner <eduardood15@hotmail.com>
2022-01-28 18:43:21 -0300

    realizada a separação entre o serviço e o controlador, para que o controlador fique apenas sendo o link da requisição para a lógica de acesso à base

---
+++ personapi/src/main/java/com/dio/personapi/service/PersonService.java
@@ -0,0 +1,29 @@
+package com.dio.personapi.service;
+
+
+import com.dio.personapi.dto.MessageResponseDTO;
+import com.dio.personapi.entity.Person;
+import com.dio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
+}
