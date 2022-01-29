ba446470352bc5c1a6c481786261de0b75f12a6f
edemuner <eduardood15@hotmail.com>
2022-01-29 11:50:35 -0300

    desenvolvida a operação PUT

--- personapi/src/main/java/com/dio/personapi/controller/PersonController.java
+++ personapi/src/main/java/com/dio/personapi/controller/PersonController.java
@@ -38,6 +38,11 @@ public class PersonController {
         return personService.findById(id);
     }
 
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable @Valid Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
