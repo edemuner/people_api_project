0fcaa7cc406c15f4da0bb18bba0bf4856ee5b5aa
edemuner <eduardood15@hotmail.com>
2022-01-29 16:47:09 -0300

    implementado um atalho do lombok nas classes PersonController e PersonService para encurtar o código dedicado aos construtores - criado README.md

--- personapi/src/main/java/com/dio/personapi/service/PersonService.java
+++ personapi/src/main/java/com/dio/personapi/service/PersonService.java
@@ -7,6 +7,7 @@ import com.dio.personapi.entity.Person;
 import com.dio.personapi.exception.PersonNotFoundException;
 import com.dio.personapi.mapper.PersonMapper;
 import com.dio.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository){
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
 
