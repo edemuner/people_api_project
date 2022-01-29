c77f5b01ea4e867051b788116aa25e6a51bc6cd1
edemuner <eduardood15@hotmail.com>
2022-01-29 17:31:44 -0300

    elaborado teste unitário para a criação de objetos Person e a respectiva mensagem

---
+++ personapi/src/test/java/com/dio/personapi/service/PersonServiceTest.java
@@ -0,0 +1,50 @@
+package com.dio.personapi.service;
+
+
+import com.dio.personapi.dto.MessageResponseDTO;
+import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.entity.Person;
+import com.dio.personapi.repository.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.dio.personapi.utils.PersonUtils.createFakeDTO;
+import static com.dio.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        Mockito.when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedMessageResponse(expectedSavedPerson.getId());
+
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedMessageResponse(Long id) {
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + id)
+                .build();
+    }
+}
